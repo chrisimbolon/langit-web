72c21e586bec0886a1810720a66a2d030be54056
Christyan Simbolon <sambungdata@gmail.com>
2026-05-20 23:59:21 +0700

    chore: add Not Found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-neutral-50">
+      <div className="text-center">
+        <h1 className="text-6xl font-light mb-6">404</h1>
+        <p className="text-neutral-600">
+          The page you’re looking for does not exist.
+        </p>
+      </div>
+    </section>
+  );
+}%
