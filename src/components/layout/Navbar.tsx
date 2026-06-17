5898753798b2f6fdc7ff62f597c78037fc1ff174
Christyan Simbolon <sambungdata@gmail.com>
2026-06-17 17:38:03 +0700

    feat: update logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.png"
+import logo from "@/assets/logo-big.png"
 import i18n from "@/lib/i18n"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
